96bfd2c285c0a1a9cebdf0b3fc46d3cca150b5ea
wzg <wzg23262@163.com>
2021-05-10 14:52:50 +0800

    2

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,18 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    // 用于切换闭合菜单的状态  flase不闭合 true 闭合
+    collapsed: false,
   },
   mutations: {
+    changeCollapsed(state) {
+      state.collapsed = !state.collapsed;
+    },
   },
   actions: {
+    changeCollapsed({ commit }) {
+      commit('changeCollapsed');
+    },
   },
   modules: {
   },
