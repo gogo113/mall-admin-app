320f89cda93bc8d06913fc816a10a93b297b4ba1
wzg <wzg23262@163.com>
2021-05-11 20:40:16 +0800

    6

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,8 @@ const ayncRouterMap = [{
   name: 'Produce',
   meta: {
     title: '商品',
+    icon: 'inbox',
+    hidden: false,
   },
   component: Home,
   children: [{
@@ -18,6 +20,8 @@ const ayncRouterMap = [{
     name: 'ProductList',
     meta: {
       title: '商品列表',
+      icon: 'unordered-list',
+      hidden: false,
     },
     component: () => import('../views/page/productList.vue'),
   }, {
@@ -25,6 +29,8 @@ const ayncRouterMap = [{
     name: 'ProductAdd',
     meta: {
       title: '添加商品',
+      icon: 'file-add',
+      hidden: false,
     },
     component: () => import('../views/page/productAdd.vue'),
   }, {
@@ -32,6 +38,8 @@ const ayncRouterMap = [{
     name: 'Category',
     meta: {
       title: '类目管理',
+      icon: 'project',
+      hidden: false,
     },
     component: () => import('../views/page/category.vue'),
   }],
@@ -43,12 +51,16 @@ const routes = [
     component: Home,
     meta: {
       title: '首页',
+      icon: 'home',
+      hidden: false,
     },
     children: [{
       path: 'index',
       name: 'Index',
       meta: {
         title: '统计',
+        icon: 'number',
+        hidden: false,
       },
       component: () => import('../views/page/index.vue'),
     }],
@@ -58,17 +70,10 @@ const routes = [
     name: 'Login',
     meta: {
       title: '登录',
+      hidden: true,
     },
     component: Login,
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  // },
 ];
 
 const router = new VueRouter({
@@ -82,9 +87,11 @@ router.beforeEach((to, from, next) => {
     if (store.state.user.appkey && store.state.user.username && store.state.user.role) {
       if (!isAddRoutes) {
         const menuRoutes = getMenuRoutes(store.state.user.role, ayncRouterMap);
+        store.dispatch('changeMenuRoutes', routes.concat(menuRoutes)).then(() => {
         // console.log(menuRoutes);
-        router.addRoutes(menuRoutes);
-        store.dispatch('changeMenuRoutes', routes.concat(menuRoutes));
+          router.addRoutes(menuRoutes);
+          next();
+        });
         isAddRoutes = true;
       }
       return next();
